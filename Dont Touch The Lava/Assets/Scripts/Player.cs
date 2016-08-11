ec404e362543e80efde9a4fd8b5ffc4024e54a5e
The Steve <thesteveiam@gmail.com>
2016-08-10 23:30:22 -0500

    Great progress today, even if it was just an hour of work! launching and landing, resetting position, some rotational force fun that I'll play with later, next up camera following and platform generation!

--- Dont Touch The Lava/Assets/Scripts/Player.cs
+++ Dont Touch The Lava/Assets/Scripts/Player.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class Player : MonoBehaviour
 {
 
-    bool dragging;
+    bool dragging, flying, launched; //maybe change to state enum?
     float maxDrag = 3f;
     float forceMultiplier = 4f;
-    Vector3 startPos, mouseStart;
+    Vector3 startPos, mouseStart, lastPos;
     Camera cam;
     Rigidbody body;
-    Transform fireLine;
+    Transform fireLine, lastPlatform;
 
     // Use this for initialization
     void Start()
@@ -25,39 +25,64 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (dragging)
+        if (dragging && !flying)
         {
             deltaPos = startPos - Vector3.ClampMagnitude(cam.ScreenToWorldPoint(mouseStart) - cam.ScreenToWorldPoint(Input.mousePosition), maxDrag);
             deltaPos.y = transform.position.y;
             transform.position = deltaPos;
             deltaPos.y = transform.position.y - 2f;
+            transform.LookAt(startPos + new Vector3(0, 2, 0));
             //Vector3 fireDrawLine = startPos - deltaPos;
             //fireDrawLine.y = 2f;
             //fireLine.LookAt(fireDrawLine, Vector3.up);
         }
-        //else
-        //{
-        //    deltaPos = transform.position;
-        //    startPos = deltaPos;
-        //    mouseStart = deltaPos;
-        //}
+        else if (flying)
+        {
+            //transform.LookAt(transform.position + body.velocity);
+
+            if (body.velocity != Vector3.zero && !launched)
+            {
+                launched = true;
+            }
+
+            if (launched && body.velocity == Vector3.zero)
+            {
+                launched = false;
+                flying = false;
+                lastPos = new Vector3(lastPlatform.position.x, transform.position.y, lastPlatform.position.z);
+                transform.position = lastPos;
+                transform.rotation = Quaternion.Euler(Vector3.forward);
+            }
+        }
+
+        if(transform.position.y <= -10f)
+        {
+            transform.position = lastPos;
+            body.velocity = Vector3.zero;
+        }
     }
 
     void OnMouseDown()
     {
-        startPos = transform.position;
-        mouseStart = Input.mousePosition;
-        dragging = true;
-        body.isKinematic = true;
+        if (!flying)
+        {
+            startPos = transform.position;
+            mouseStart = Input.mousePosition;
+            dragging = true;
+            body.isKinematic = true;
+        }
     }
 
     void OnMouseUp()
     {
-        dragging = false;
-        //startPos = transform.position;
-        //mouseStart = transform.position;
-        body.isKinematic = false;
-        Launch();
+        if (!flying)
+        {
+            dragging = false;
+            //startPos = transform.position;
+            //mouseStart = transform.position;
+            body.isKinematic = false;
+            Launch();
+        }
     }
 
     void Launch()
@@ -65,13 +90,20 @@ public class Player : MonoBehaviour
         Vector3 shotForce = -deltaPos + startPos;
         //shotForce.y = -1000f;
         body.AddForce(shotForce * forceMultiplier, ForceMode.Impulse);
+        body.AddTorque(transform.right * 0.3f, ForceMode.Impulse);
+        flying = true;
     }
 
-    void OnDrawGizmos()
+    void OnCollisionEnter(Collision col)
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(startPos, deltaPos);
-        Gizmos.DrawWireSphere(startPos, 1f);
-        Gizmos.DrawWireSphere(deltaPos, 2f);
+        lastPlatform = col.transform;
     }
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawLine(startPos, deltaPos);
+    //    Gizmos.DrawWireSphere(startPos, 1f);
+    //    Gizmos.DrawWireSphere(deltaPos, 2f);
+    //}
 }
